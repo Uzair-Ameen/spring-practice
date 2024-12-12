6eb24e07f314d4f65da72a21a33f8a00dc91ee33
Uzair Ameen <ameenuzair98@gmail.com>
2024-12-13 00:12:23 +0500

    Added spring security

--- src/main/java/com/practice/springpractice/entities/AppUser.java
+++ src/main/java/com/practice/springpractice/entities/AppUser.java
@@ -6,16 +6,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class AppUser {
+public class AppUser implements UserDetails {
 
     public AppUser(String username, String password, String firstName, String lastName) {
         this.username = username;
@@ -31,8 +35,10 @@ public class AppUser {
     @Column(unique = true, nullable = false)
     private String username;
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private String firstName;
 
     private String lastName;
@@ -57,5 +63,30 @@ public class AppUser {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
         return encoder.matches(password, this.password);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 }
---
+++ src/main/java/com/practice/springpractice/services/JwtService.java
@@ -0,0 +1,9 @@
+package com.practice.springpractice.services;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+}
