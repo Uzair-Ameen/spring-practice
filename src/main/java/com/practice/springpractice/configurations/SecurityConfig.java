7286f957f6110bdc0c570158f2f3516c648725bc
Uzair Ameen <ameenuzair98@gmail.com>
2024-12-13 20:31:54 +0500

    Removed basic spring security config

--- src/main/java/com/practice/springpractice/configurations/SecurityConfig.java
+++ src/main/java/com/practice/springpractice/configurations/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +13,6 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll()).build();
+        return http.httpBasic(HttpBasicConfigurer::disable).build();
     }
 }
